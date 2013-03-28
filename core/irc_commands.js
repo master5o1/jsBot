5975212b88c805eee924e0e8bd7cc16cf8dffa7f
Jason Schwarzenberger <jason@master5o1.com>
2013-03-29 09:54:16 +1300

    Add OP parting from channel and muting the bot.
    Markov probability is now relative to the channel or user who set the
    probability.
    OPs can part the bot from their channel.
    OPs can mute the bot from their channel.

--- core/irc_commands.js
+++ core/irc_commands.js
@@ -2,6 +2,8 @@ var Module = module.exports = function Module(bot){
 	var self = this;
 	self.bot = bot;
 	
+	self.mute = {};
+	
 	self.load();
 	
 	console.log('core module irc_commands loaded');
@@ -14,6 +16,8 @@ Module.prototype.load = function(){
 	self.partHandler = self.part();
 	self.kickHandler = self.kick();
 	self.nickHandler = self.nick();
+	self.partOpHandler = self.opPart();
+	self.opMuteHandler = self.opMute();
 	
 	self.sayHandler = self.say();
 	
@@ -21,6 +25,8 @@ Module.prototype.load = function(){
 	self.bot.registerCommand('part', self.partHandler, 'admin');
 	self.bot.registerCommand('nick', self.nickHandler, 'admin');
 	self.bot.registerCommand('kick', self.kickHandler, 'op');
+	self.bot.registerCommand('part_op', self.partOpHandler, 'op');
+	self.bot.registerCommand('mute', self.opMuteHandler, 'op');
 	
 	self.bot.registerCommand('say', self.sayHandler, false);
 	
@@ -33,6 +39,8 @@ Module.prototype.unload = function() {
 	self.bot.deregisterCommand('part', self.partHandler);
 	self.bot.deregisterCommand('nick', self.nickHandler);
 	self.bot.deregisterCommand('kick', self.kickHandler);
+	self.bot.deregisterCommand('part_op', self.partOpHandler);
+	self.bot.deregisterCommand('mute', self.opMuteHandler);
 	
 	self.bot.deregisterCommand('say', self.sayHandler);
 };
@@ -56,13 +64,46 @@ Module.prototype.part = function(){
 	var self = this;
 	return function(client, from, to, args) {
 		var channel;
-		if (args.length == 0) {
+		if ((args.length == 0 && !self.bot.startsWith(to, '#')) || (args.length > 0 && !self.bot.startsWith(args[0], '#'))) {
 			message = self.bot.help("part", "[#channel]");
 			self.say(client, from, to, message.split(' '));
 			return;
 		}
-		channel = args[0];
-		if (!channel && self.bot.startsWith(to, '#')) channel = to;
+		if (args.length == 0) {
+			channel = to;
+		} else {
+			channel = args[0];
+		}
+		client.part(channel);
+		console.log('Parted channel', channel, 'on server', client.opt.server);
+	};
+};
+
+Module.prototype.opMute = function(){
+	var self = this;
+	return function(client, from, to, args) {
+		if (!self.bot.startsWith(to, '#')) {
+			self.bot.say(client, from, self.bot.help('mute', '[reset]'));
+			return;
+		}
+		var mute_key = client.opt.server + '/' + to;
+		self.mute[mute_key] = true;
+		if (args.length > 0 && args[0] == 'reset') {
+			self.mute[mute_key] = false;
+		}
+	};
+};
+
+Module.prototype.opPart = function(){
+	var self = this;
+	return function(client, from, to, args) {
+		var channel;
+		if (args.length == 0 && !self.bot.startsWith(to, '#')) {
+			message = self.bot.help("part", "[#channel]");
+			self.say(client, from, to, message.split(' '));
+			return;
+		}
+		channel = to;
 		client.part(channel);
 		console.log('Parted channel', channel, 'on server', client.opt.server);
 	};
@@ -104,6 +145,10 @@ Module.prototype.say = function(){
 	return function(client, from, to, args) {
 		var receiver, message;
 		message = args.join(' ');
+		var mute_key = client.opt.server + '/' + to;
+		if (typeof self.mute[mute_key] != 'undefined' && self.mute[mute_key] === true) {
+			return;
+		}
 		if (args.length == 0) {
 			message = self.bot.help("say", "<message>");
 		}
--- modules/markov.js
+++ modules/markov.js
@@ -2,128 +2,14 @@ var Mongolian = require('mongolian');
 
 var Module = module.exports = function Module(bot){
 	var self = this;
-	self.bot = bot;
+	var dbMarkov = bot.dbDatabase.collection('markov');
+	var default_probability = 0.02;
+	var max_words = 25;
+	var custom_probabilities = {}; // { 'irc.freenode.net/#ualug': 0.00 }
 	
-	self.probability = 0.02;
-	self.max_words = 50;
-	
-	self.dbMarkov = self.bot.dbDatabase.collection('markov');
-	
-	self.load();
-	
-	console.log('module markov loaded');
-};
-
-Module.prototype.load = function(){
-	var self = this;
-	
-	self.builderHandler = self.builder();
-	self.probabilityHandler = self.probable();
-	self.setProbHandler = self.setProbability();
-	self.gatherHandler = self.gather();
-	
-	self.bot.registerCommand('markov', self.builderHandler);
-	self.bot.registerCommand('markov_prob', self.setProbHandler);
-	self.bot.addListener('message', self.probabilityHandler);
-	self.bot.addListener('message', self.gatherHandler);
-};
-
-Module.prototype.unload = function() {
-	var self = this;
-	
-	self.bot.deregisterCommand('markov', self.builderHandler);
-	self.bot.deregisterCommand('markov_prob', self.setProbHandler);
-	self.bot.removeListener('message', self.probabilityHandler);
-	self.bot.removeListener('message', self.gatherHandler);
-};
-
-Module.prototype.builder = function(){
-	var self = this;
-	return function(client, from, to, args){
-		var reply = '',
-			dict = {},
-			receiver = self.bot.startsWith(to, '#') ? to : from;
-		// console.log('Building Markov String');
-		
-		if (args.length > 0 && args[0])
-			reply = from + ':';
-		
-		function build_string(dict, length) {
-			var dict_keys = [];
-			length = length || self.max_words;
-			for (var k in dict) { dict_keys.push(k); }
-			var start_key = Math.round(Math.random()*dict_keys.length)%dict_keys.length;
-			var key = dict_keys[start_key];
-			var str = key;
-			while (str.split(" ").length < length) {
-				var value = dict[key];
-				str = str + " " + value;
-				key = key.split(" ")[1] + " " + value;
-				if (typeof dict[key] == 'undefined') { break; }
-			}
-			return str;
-		};
-		
-		self.dbMarkov.count(function(err, count){
-			var skip = Math.round(Math.random() * count+1) % count;
-			var cursor = self.dbMarkov.find({}).skip(skip).limit(1);
-			function findWords(cursor) {
-				cursor.toArray(function(err, data){
-					var item, next_key, next_word, build_str;
-					if (err) return;
-					if (data.length == 0) {
-						if (dict) {
-							var build_str = build_string(dict).split(' ');
-							if (reply.length > 0) build_str.unshift(reply);
-							self.bot.emit('command_say', client, self.bot.details.nick, receiver, build_str);
-						}
-						return
-					};
-					item = data[0];
-					if (item.words.length == 0) {
-						return;
-					} else {
-						next_word = Math.round(Math.random()*item.words.length) % item.words.length;
-						next_key = item.key.split(' ').concat(item.words[next_word]).slice(1).join(' ');
-						dict[item.key] = item.words[next_word];
-						findWords(self.dbMarkov.find({key: next_key}).limit(1));
-					}
-				});
-			}
-			findWords(cursor);
-		});
-	};
-};
-
-Module.prototype.probable = function(){
-	var self = this;
-	return function(client, from, to, message) {
-		if (Math.random() < self.probability)
-			self.bot.emit('command_markov', client, from, to, []);
-	};
-};
-
-Module.prototype.setProbability = function(){
-	var self = this;
-	return function(client, from, to, args) {
-		var receiver = self.bot.startsWith(to, '#') ? to : from;
-		var text = "Random markov generation probability is set to " + self.probability.toFixed(2) + ".";
-		if (args.length == 0) {
-			text = self.bot.help('markov_prob', "<float>");
-		} else if (/number/i.test(typeof parseFloat(args[0])) && ! /nan/i.test(parseFloat(args[0]).toString())) {
-			self.probability = parseFloat(args[0]);
-			text = "Random markov generation probability is set to " + self.probability.toFixed(2) + ".";
-		}
-		self.bot.emit('command_say', client, self.bot.details.nick, receiver, text.split(' '));
-	};
-};
-
-Module.prototype.gather = function(){
-	var self = this;
-	return function(client, from, to, message) {
+	var gather = function(client, from, to, message) {
 		var dict, i, words, first, second, third, key;
 		if (message.length < 3) return;
-		// console.log('Adding words to Markov DB');
 		
 		dict = {};
 		i = 0;
@@ -140,19 +26,158 @@ Module.prototype.gather = function(){
 		};
 		
 		Object.keys(dict).forEach(function(key){
-			self.dbMarkov.findOne({ key: key }, function(err, item){
+			dbMarkov.findOne({ key: key }, function(err, item){
 				var updated_words = [];
 				if (typeof item == 'undefined') {
-					self.dbMarkov.insert({
+					dbMarkov.insert({
 						key: key,
 						words: dict[key]
 					});
 				} else {
 					updated_words = dict[key].concat(item.words);
 					item.words = updated_words;
-					self.dbMarkov.save(item);
+					dbMarkov.save(item);
 				}
 			});
 		});
 	};
+	
+	var setProbability = function(client, from, to, args) {
+		var receiver = bot.startsWith(to, '#') ? to : from;
+		var prob_key = client.opt.server + '/' + receiver;
+		var probability = typeof custom_probabilities[prob_key] == 'number' ? custom_probabilities[prob_key] : default_probability;
+		var text = 'Markov probability for ' + prob_key + ' is set to ' + probability.toFixed(2) + '.';
+		if (args.length == 0) {
+			text = bot.help('markov_prob', "<float>");
+		} else if (/number/i.test(typeof parseFloat(args[0])) && ! /nan/i.test(parseFloat(args[0]).toString())) {
+			custom_probabilities[prob_key] = parseFloat(args[0]);
+			probability = typeof custom_probabilities[prob_key] == 'number' ? custom_probabilities[prob_key] : default_probability;
+			text = 'Markov probability for ' + prob_key + ' is set to ' + probability.toFixed(2) + '.';
+		}
+		bot.say(client, receiver, text);
+	};
+	
+	var listProbability = function(client, from, to, args) {
+		var text = 'Markov Probabilities: default = ' + default_probability + '\n';
+		text += Object.keys(custom_probabilities).map(function(key){
+			return key + ' has probability of ' + custom_probabilities[key];
+		}).join('\n');
+		bot.say(client, from, text);
+	};
+	
+	var builder = function(client, from, to, args){
+		var dict = {},
+			receiver = bot.startsWith(to, '#') ? to : from;
+		
+		function build_string(dict, length) {
+			var dict_keys = [];
+			length = length || max_words;
+			for (var k in dict) { dict_keys.push(k); }
+			var start_key = Math.round(Math.random()*dict_keys.length)%dict_keys.length;
+			var key = dict_keys[start_key];
+			var str = key;
+			while (str.split(" ").length < length) {
+				var value = dict[key];
+				str = str + " " + value;
+				key = key.split(" ")[1] + " " + value;
+				if (typeof dict[key] == 'undefined') { break; }
+			}
+			return str;
+		};
+		
+		dbMarkov.count(function(err, count){
+			function findWords(cursor, cursor_default) {
+				cursor.toArray(function(err, data){
+					var item, next_key, next_word;
+					if (err) return;
+					if (data.length == 0 && typeof cursor_default != 'undefined') {
+						findWords(cursor_default);
+						return;
+					}
+					if (data.length == 0) {
+						if (dict) {
+							bot.say(client, receiver, build_string(dict));
+						}
+						return
+					};
+					item = data[0];
+					if (item.words.length == 0) {
+						return;
+					} else {
+						next_word = Math.round(Math.random()*item.words.length) % item.words.length;
+						next_key = item.key.split(' ').concat(item.words[next_word]).slice(1).join(' ');
+						dict[item.key] = item.words[next_word];
+						findWords(dbMarkov.find({key: next_key}).limit(1));
+					}
+				});
+			}
+			var skip = Math.round(Math.random() * count+1) % count,
+				starting_key = {},
+				cursor = dbMarkov.find(starting_key).skip(skip).limit(1),
+				default_cursor = dbMarkov.find(starting_key).skip(skip).limit(1);
+			if (args.length > 0) {
+				start = Math.round(Math.random() * (args.length + 1)) % args.length;
+				finish = start + 2;
+				cursor = dbMarkov.find({ key: args.slice(start, finish).join(' ') }).limit(1);
+			}
+			findWords(cursor, default_cursor);
+		});
+	};
+	
+	var markovCommand = function(client, from, to, args){
+		var receiver = bot.startsWith(to, '#') ? to : from;
+		if (args.length == 0) {
+			bot.say(client, receiver, bot.help('markov', '(list|say <words>|probability <float>)'));
+			return
+		}
+		if (args[0] == 'list') {
+			listProbability(client, from, to, args);
+		} else if (args[0] == 'say') {
+			builder(client, from, to, args.slice(1));
+		} else if (args[0] == 'probability' && args.length > 1) {
+			setProbability(client, from, from, args.slice(1)); // users can only set their own one.
+		}
+	};
+	
+	var markovOpCommand = function(client, from, to, args){
+		if (!bot.startsWith(to, '#')) {
+			bot.say(client, receiver, 'Say this command from a channel that you are op in');
+			return
+		}
+		if (args.length == 0) {
+			bot.say(client, receiver, bot.help('markov_op', '<float>'));
+			return;
+		}
+		setProbability(client, from, to, args); // users can only set their own one.
+	};
+	
+	var onProbability = function(client, from, to, message) {
+		var receiver = bot.startsWith(to, '#') ? to : from;
+		var prob_key = client.opt.server + '/' + receiver;
+		var probability = typeof custom_probabilities[prob_key] == 'number' ? custom_probabilities[prob_key] : default_probability;
+		if (Math.random() <= probability)
+			builder(client, from, to, message.split(' '));
+	};
+	
+	this.load = function(){
+		bot.addListener('message', onProbability);
+		bot.addListener('message', gather);
+		
+		bot.registerCommand('markov_op', markovOpCommand, 'op');
+		bot.registerCommand('markov', markovCommand);
+		
+		console.log('module markov loaded');
+	};
+	
+	this.unload = function() {	
+		bot.removeListener('message', onProbability);
+		bot.removeListener('message', gather);
+		
+		bot.deregisterCommand('markov_op', markovOpCommand);
+		bot.deregisterCommand('markov', markovCommand);
+		
+		console.log('module markov unloaded');
+	};
+	
+	this.load();
 };
